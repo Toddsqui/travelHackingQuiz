5383485c9e5aab0df14f578e076ca46055a9852d
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2016-01-10 13:20:53 +0900

    Edited the innerHTML on the page after the user submits the results.

--- js/script.js
+++ js/script.js
@@ -22,26 +22,16 @@ function submitAnswers(){
     var answers = ["c","b","c","a","c"];
 
     // check answers and give points
-    if(q1 == answers[0]){
+    for(var i = 1; i <= total; i++){
+      if(eval('q'+ i) == answers[i - 1]){
       score++; // this will give a point to the student who gets the question correctly
     }
+}
 
-    if(q2 == answers[1]){
-      score++; // this will give a point to the student who gets the question correctly
-    }
+  // display results
+  var results = document.getElementById('results');
+  results.innerHTML = "<h3>You scored <span>"+ score + "</span> out of <span>" + total + "</span></h3>";
+  alert('You scored ' + score + " out of " + total + "!");
 
-    if(q3 == answers[2]){
-      score++; // this will give a point to the student who gets the question correctly
-    }
-
-    if(q4 == answers[3]){
-      score++; // this will give a point to the student who gets the question correctly
-    }
-
-    if(q5 == answers[4]){
-      score++; // this will give a point to the student who gets the question correctly
-    }
-
-    alert('You scored ' + score + " out of " + total + "!");
-
+  return false;
 }
